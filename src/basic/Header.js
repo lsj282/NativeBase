bfc192f48dc606e1f25ced78e91ea8948d29246e
lsj282 <99780455+lsj282@users.noreply.github.com>
2023-11-27 13:49:28 +0900

    Update Header.js

--- src/basic/Header.js
+++ src/basic/Header.js
@@ -21,94 +21,17 @@ class Header extends Component {
     };
   }
 
-  layoutChange(val) {
-    const maxComp = Math.max(variable.deviceWidth, variable.deviceHeight);
-
-    if (val.width >= maxComp) this.setState({ orientation: 'landscape' });
-    else this.setState({ orientation: 'portrait' });
-  }
-
-  calculateHeight(mode, inSet) {
-    const { style } = this.props;
-    let inset = null;
-
-    if (inSet !== undefined) {
-      inset = inSet;
-    } else {
-      inset = variable.Inset;
-    }
-
-    const InsetValues = mode === 'portrait' ? inset.portrait : inset.landscape;
-    let oldHeight = null;
-
-    if (style.height !== undefined) {
-      oldHeight = style.height;
-    } else if (style[1]) {
-      oldHeight = style[1].height ? style[1].height : style[0].height;
-    } else {
-      oldHeight = style[0].height;
-    }
-
-    const height = oldHeight + InsetValues.topInset;
-
-    return height;
-  }
-
-  calculatePadder(mode, inSet) {
-    let inset = null;
-
-    if (inSet !== undefined) {
-      inset = inSet;
-    } else {
-      inset = variable.Inset;
-    }
-
-    const InsetValues = mode === 'portrait' ? inset.portrait : inset.landscape;
-    let topPadder = null;
-    const style = StyleSheet.flatten(this.props.style);
-
-    if (style.padding !== undefined || style.paddingTop !== undefined) {
-      topPadder =
-        (style.paddingTop ? style.paddingTop : get(style, 'padding', 0)) +
-        InsetValues.topInset;
-    } else {
-      topPadder = InsetValues.topInset;
-    }
-
-    return topPadder;
-  }
 
   render() {
     const {
-      androidStatusBarColor,
-      iosBarStyle,
       style,
-      transparent,
-      translucent
     } = this.props;
     const { orientation } = this.state;
-    const variables = this.context.theme
-      ? this.context.theme['@@shoutem.theme/themeStyle'].variables
-      : variable;
-    const platformStyle = variables.platformStyle;
+  
 
     return (
-      <View onLayout={e => this.layoutChange(e.nativeEvent.layout)}>
-        {variables.isIphoneX ? (
-          <View
-            ref={c => (this._root = c)}
-            {...this.props}
-            style={[
-              style,
-              {
-                height: this.calculateHeight(orientation, variables.Inset),
-                paddingTop: this.calculatePadder(orientation, variables.Inset)
-              }
-            ]}
-          />
-        ) : (
+      <View>
           <View ref={c => (this._root = c)} {...this.props} />
-        )}
       </View>
     );
   }
